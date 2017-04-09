48bf52e388061262f88a6f0ec8ee1cac0f8096fe
Torello Querci <tquerci lgmail.com>
2017-04-09 12:28:37 +0200

    Sharing actiovity and messaging added

--- App/VolumioApp/app/src/main/java/volapp/droidcon/org/volumioapp/services/VolumioQueue.java
+++ App/VolumioApp/app/src/main/java/volapp/droidcon/org/volumioapp/services/VolumioQueue.java
@@ -29,6 +29,8 @@ public class VolumioQueue extends IntentService {
     public static final String ACTION_OPEN_CONNECTION = "openConnection";
     public static final String ACTION_GET_STATE = "getState";
     public static final String ACTION_GET_QUEUE = "getQueue";
+    public static final String ACTION_PLAY_URL = "playUrl";
+    public static final String ACTION_GET_CURRENT_STATUS = "getCurrentStatus";
 
     public VolumioQueue() {
         super("VolumioQueue");
@@ -108,6 +110,19 @@ public class VolumioQueue extends IntentService {
         context.startService(intent);
     }
 
+    public static void startPlayingUrl(Context context, String url) {
+        Intent intent = new Intent(context, VolumioQueue.class);
+        intent.setAction(ACTION_PLAY_URL);
+        intent.putExtra("URL", url);
+        context.startService(intent);
+    }
+
+    public static void ReadCurrentStatus(Context context) {
+        Intent intent = new Intent(context, VolumioQueue.class);
+        intent.setAction(ACTION_GET_CURRENT_STATUS);
+        context.startService(intent);
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
@@ -121,6 +136,13 @@ public class VolumioQueue extends IntentService {
                 break;
             case ACTION_GET_QUEUE:
                 getQueue ();
+                break;
+            case ACTION_PLAY_URL:
+                String url = intent.getExtras().getString("URL");
+                playUrl (url);
+                break;
+            case ACTION_GET_CURRENT_STATUS:
+                SendCurrentStatus();
                 break;
             }
         }
@@ -137,6 +159,21 @@ public class VolumioQueue extends IntentService {
     private void getQueue() {
         if (CurrentState == STATE.CONNECTED) {
             socket.emit("getQueue", "[]");
+        } else {
+            // Do something
+        }
+    }
+
+    private void playUrl(String url) {
+        if (CurrentState == STATE.CONNECTED) {
+            try {
+
+                JSONObject obj = new JSONObject();
+                obj.put("endpoint", "music_service/PiYoutubeQueue");
+                obj.put("method", "play");
+                obj.put("data", url);
+                socket.emit("callMethod", obj);
+            } catch (Exception ex) {}
         } else {
             // Do something
         }
--- App/VolumioApp/app/src/main/java/volapp/droidcon/org/volumioapp/ui/SharingActivity.kt
+++ App/VolumioApp/app/src/main/java/volapp/droidcon/org/volumioapp/ui/SharingActivity.kt
@@ -1,14 +1,27 @@
 package volapp.droidcon.org.volumioapp.ui
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.json.JSONArray
 import volapp.droidcon.org.volumioapp.R
 import volapp.droidcon.org.volumioapp.connectivity.WifiConnectionService
+import volapp.droidcon.org.volumioapp.model.Song
+import volapp.droidcon.org.volumioapp.model.SongsAdapter
+import volapp.droidcon.org.volumioapp.services.VolumioQueue
 
 class SharingActivity : AppCompatActivity(), AnkoLogger {
+
+    var linkToShare: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +30,39 @@ class SharingActivity : AppCompatActivity(), AnkoLogger {
 //        Connects to the Network
         WifiConnectionService.startActionConnect(this)
         if(intent.action == Intent.ACTION_SEND){
-            val linkToShare = intent.extras[Intent.EXTRA_TEXT]
+            linkToShare = intent.extras[Intent.EXTRA_TEXT].toString()
             info("Link to share is $linkToShare")
         }
     }
 
-    fun shareTheLink(){
-        TODO("Implement the shareLink")
+    //fun shareTheLink(){
+    //    TODO("Implement the shareLink")
+    //}
+
+    override fun onPause() {
+        // Unregister since the activity is not visible
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver)
+        super.onPause()
     }
+
+    public override fun onResume() {
+        super.onResume()
+
+        // Register mMessageReceiver to receive messages.
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                IntentFilter("CONNECTION-STATUS-UPDATE"))
+    }
+    // handler for received Intents for the "my-event" event
+    private val mMessageReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val message = intent.getStringExtra("status")
+            when (message) {
+                "CONNECTED" -> {
+                    VolumioQueue.startPlayingUrl(this@SharingActivity, linkToShare)
+                    this@SharingActivity.finish()
+                }
+            }
+        }
+    }
+
 }
